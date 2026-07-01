34114bd3acbd10acc59c84fb489c32a87fd028ef
ITAlonsoRabanal <nachoalonso273@gmail.com>
2026-07-01 19:45:47 -0300

    fix routes

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,10 @@ window.addEventListener('load', () => {
     const initButon = document.getElementById("initButon")
     initButon.addEventListener("click", () => {
         globalOnload(true)
-        window.location.href = "/src/pages/login.html?reason=init_complete";
+        window.location.href = "src/pages/login.html?reason=init_complete";
     })
     const goLogin = document.getElementById("goLogin")
     goLogin.addEventListener("click", () => {
-        window.location.href = "/src/pages/login.html"
+        window.location.href = "src/pages/login.html"
     })
 })
